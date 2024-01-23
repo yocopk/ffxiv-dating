e38088c820224473556c51c0a0c8222bfc9af38d
yocopk <yocopk@gmail.com>
2024-01-23 02:04:36 +0100

    update background and service section

--- src/components/Jumbo.jsx
+++ src/components/Jumbo.jsx
@@ -1,22 +1,22 @@
 export default function Jumbo() {
   return (
-    <section className="bg-gradient-to-r from-red-300 to-pink-400 h-screen pt-14 md:pl-32">
+    <section className="bg-[url(src/assets/ffxiv-landscape.png)] bg-cover h-screen pt-14 md:pl-32">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div className="mr-auto place-self-center lg:col-span-7">
+        <div className="mx-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl text-black font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
             TINFANTASY
           </h1>
           <h2 className="max-w-xl mb-4 text-2xl text-black/60 font-bold tracking-tight leading-none md:text-3xl">
             The Final Fantasy XIV Dating Website
           </h2>
-          <p className="max-w-2xl mb-6 font-light text-black lg:mb-8 md:text-lg lg:text-xl">
+          <p className="max-w-2xl mb-6 text-black lg:mb-8 md:text-lg lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Consequuntur repudiandae non maiores est repellat iste, quam quis,
             minus incidunt ullam maxime magni explicabo aperiam quaerat eos.
           </p>
           <a
             href="#"
-            className="group bg-white inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-black rounded-lg"
+            className="group bg-white inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-black rounded-lg z-10"
           >
             Get started
             <svg
@@ -35,7 +35,7 @@ export default function Jumbo() {
             Tell me more..
           </a>
         </div>
-        <div className="w-[220px] md:w-[270px] mx-auto md:mx-0 h-auto lg:mt-0 lg:col-span-5 lg:flex">
+        <div className="w-[210px] md:w-[270px] xl:w-[340px] mx-auto md:mx-0 h-auto lg:mt-0 lg:col-span-5 lg:flex">
           <img
             className="pt-6"
             src="src\assets\smartphone-jumbo.png"
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,3 +1,47 @@
 export default function Services() {
-  return <div className="bg-yellow-500 h-72 w-full"></div>;
+  return (
+    <div className="bg-white flex flex-col md:flex-row items-center gap-4 md:h-auto w-full py-6 md:py-16">
+      <div className="flex px-10">
+        <div className="text-black bg-gray-200 h-[50%] text-4xl p-1 rounded-lg mr-4">
+          <ion-icon name="checkmark-done-outline"></ion-icon>
+        </div>
+        <div className="text-black">
+          <h2 className="text-4xl font-medium">Verified Members.</h2>
+          <p className="pt-2">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+            aliquam dolores iste officiis odit dolore enim quam libero? Odit aut
+            facere.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex px-10">
+        <div className="text-black bg-gray-200 h-[50%] text-4xl p-1 rounded-lg mr-4">
+          <ion-icon name="heart-outline"></ion-icon>
+        </div>
+        <div className="text-black">
+          <h2 className="text-4xl font-medium">Soul mate guaranteed.</h2>
+          <p className="pt-2">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+            aliquam dolores iste officiis odit dolore enim quam libero? Odit aut
+            facere.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex px-10">
+        <div className="text-black bg-gray-200 h-[50%] text-4xl p-1 rounded-lg mr-4">
+          <ion-icon name="bulb-outline"></ion-icon>
+        </div>
+        <div className="text-black">
+          <h2 className="text-4xl font-medium">Easy to use.</h2>
+          <p className="pt-2">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+            aliquam dolores iste officiis odit dolore enim quam libero? Odit aut
+            facere.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
